310e48423ba036b0ad97585a3e1c76f06eb737f4
Matt Payne <Payne@MattPayne.org>
2017-05-26 18:51:12 -0500

    syntax for generate was wrong.  Fixed migration by hand.

--- db/migrate/20170526234501_create_books.rb
+++ db/migrate/20170526234501_create_books.rb
@@ -1,10 +1,10 @@
-class CreateBooks < ActiveRecord::Migration[5.0]
+class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :coverImage,
-      t.string :title,
-      t.string :author,
-      t.date, :releaseDate
+      t.string :coverImage
+      t.string :title
+      t.string :author
+      t.date :releaseDate
       t.string :keywords
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605163758) do
+ActiveRecord::Schema.define(version: 20170526234501) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "coverImage"
+    t.string   "title"
+    t.string   "author"
+    t.date     "releaseDate"
+    t.string   "keywords"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
