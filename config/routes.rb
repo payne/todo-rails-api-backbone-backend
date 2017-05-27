fef68e577d109bbd4e2e39c399df1a942859f9a0
Matt Payne <Payne@MattPayne.org>
2017-05-26 19:31:58 -0500

    both backbone apps can read.  Only todo can create.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :books
+  #resources :books
+  resources :todos,:books
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
