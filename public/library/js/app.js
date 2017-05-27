afc6b0db8ee84a80590d4713509fa2673cb52a3d
Matt Payne <Payne@MattPayne.org>
2017-05-26 19:17:48 -0500

    only get books from API

--- public/library/js/app.js
+++ public/library/js/app.js
@@ -1,6 +1,8 @@
 // site/js/app.js
 var app = app || {};
-$(function() { var books = [
+$(function() { 
+	
+	var books = [
             { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford',
               releaseDate: '2008', keywords: 'JavaScript Programming' },
             { title: 'The Little Book on CoffeeScript', author: 'Alex MacCaw',
@@ -12,4 +14,4 @@ $(function() { var books = [
             { title: 'Eloquent JavaScript', author: 'Marijn Haverbeke',
               releaseDate: '2011', keywords: 'JavaScript Programming' }
 ];
-new app.LibraryView( books ); });
+new app.LibraryView( ); });
